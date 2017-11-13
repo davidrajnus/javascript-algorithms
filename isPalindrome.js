41a4dc48c11a3ff2febe1c9f8ddccda7acf1347c
DRAJ <davidrajnus@gmail.com>
2017-11-13 11:26:52 +0800

    created isPalindrome algo

---
+++ isPalindrome.js
@@ -0,0 +1,16 @@
+function isPalindrome(string) {
+  //lowercase the string and regex removes spaces and non-alphabet character
+  var cleanString = string.toLowerCase().replace(/\W/g, '');
+  
+  //reverse function works with array, hence the need to split and join the string
+  if (cleanString === cleanString.split('').reverse().join('')){
+    return true;
+  }
+  return false;
+}
+
+isPalindrome("Madam I'm Adam");
+
+//function has a Linear Time Complexity O(n) + O(m)
+//isPalindrom is a function that checks if a string is the same when it is reversed
+// example: "Madam I'm Adam" is the same forward and reverse
